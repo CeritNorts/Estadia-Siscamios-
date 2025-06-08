af9a4fe6eb169a4bb2f7da4434e74d12cf1d377e
CeritNorts <20223l001058@utcv.edu.mx>
2025-06-07 22:48:04 -0600

    Creacion de Modelos Chofer, Cliente y Viaje

--- app/Models/Viaje.php
+++ app/Models/Viaje.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Viaje extends Model
 {
-    //
+    protected $fillable = [
+        'camion_id',
+        'chofer_id',
+        'cliente_id',
+        'ruta',
+        'fecha_salida',
+        'fecha_llegada',
+        'estado',
+    ];
+
+    // Relaciones
+    public function camion()
+    {
+        return $this->belongsTo(Camion::class);
+    }
+
+    public function chofer()
+    {
+        return $this->belongsTo(Chofer::class);
+    }
+
+    public function cliente()
+    {
+        return $this->belongsTo(Cliente::class);
+    }
 }
